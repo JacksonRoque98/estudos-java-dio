40e1699b4a8a7d5a5ce05c500e2126d09437ad2f
Jackson <jackson.roque98@icloud.com>
2023-08-17 15:16:25 -0300

    Commit sobre tipos e variaveis.

---
+++ Java Basico/JavaBasicoDio/src/Number/NumericalDataType.java
@@ -0,0 +1,16 @@
+package Number;
+public class NumericalDataType {
+        double testeDouble1 = 2500;
+        //R$2.500,00
+
+        double testeDouble2 = 2.500;
+        //R$2,50
+
+        float pi = 3.14f;
+        //Ao usar float sempre colocar um F ou f no final do numero.
+
+        final double PI = 3.14;
+        //utilizar final e declarar a variavel com letra maiuscula define uma variavel que não pode ser alterada.
+
+
+}
---
+++ Java Basico/JavaBasicoDio/src/String/StringDataType.java
@@ -0,0 +1,11 @@
+package String;
+
+public class StringDataType {
+
+    String name = "Jackson";
+
+    String surName = "Roque Pereira";
+
+    String fullName = name + surName;
+
+}
